7561249328132b24277ea166881422531f0aaec1
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-13 23:21:04 -0400

    Local veriable has been set

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
