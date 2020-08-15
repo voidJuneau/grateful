abaf3a0bb5d34143ddc54df3b717af391c13add4
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-14 22:27:57 -0400

    Tried to add JWT

--- server/models/user.js
+++ server/models/user.js
@@ -28,15 +28,30 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         required: true
     }
+    // ,
+    // tokens: [{
+    //     token: {
+    //         type: String,
+    //         required: true
+    //     }
+    // }]
 })
 
 userSchema.pre('save', async function(next) {
     const user = this;
-    if (user.isModified('password') || user.isNew) {
+    if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
     next();
 })
+
+// userSchema.methods.generateAuthToken = async function() {
+//     const user = this;
+//     const token = jwt.sign({_id: user._id}, process.env.JWT_KEY);
+//     user.tokens = user.tokens.concat({token});
+//     await user.save()
+//     return token;
+// }
 
 userSchema.methods.toJSON = function() {
     const user = this;
--- server/routers/user.js
+++ server/routers/user.js
@@ -3,10 +3,11 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/api/signin', async (req, res) => {
-    const user = new User(req.body)
     try {
+        const user = new User(req.body);
         await user.save();
         // const token = await user.generateAuthToken();
+        // res.status(201).send({user, token});
         res.status(201).send({user});
     } catch (e) {
         res.status(400).send(e);
