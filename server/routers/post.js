f82320dab0ef0b4740a271b033b5154c990b497a
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-16 22:47:45 -0400

    Added post routes

--- server/routers/post.js
+++ server/routers/post.js
@@ -13,20 +13,23 @@ router.post('/api/post/new', auth, async (req, res) => {
         await post.save();
         res.status(201).send(post);
     } catch(e) {
-        res.status(400).send({error: e.message});
+        res.status(400).send(e);
     }
 })
 
 router.get('/api/post/all', async (req, res) => {
-    // Post.find({$or:[{public: true}, {owner:req.user._id}]}, (err, posts) => {
+    //(with auth) Post.find({$or:[{public: true}, {owner:req.user._id}]}, (err, posts) => {
     Post.find({public: true}, (err, posts) => {
-        if (err) return res.send(err);
-        res.send(posts)
+        if (err) return res.status(500).send(err);
+        res.send(posts);
     })
 })
 
 router.get('/api/post/mine', auth, async (req, res) => {
-
+    Post.find({owner: req.user._id}, (err, posts) => {
+        if (err) return res.status(500).send(err);
+        res.send(posts);
+    })
 })
 
 module.exports = router;
