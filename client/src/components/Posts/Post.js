270ba948221b5fbd0b7be6581d973a3c98f111ab
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-21 22:14:03 -0400

    Styled posts

--- client/src/components/Posts/Post.js
+++ client/src/components/Posts/Post.js
@@ -5,14 +5,16 @@ const Post = props => {
   const Box = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 2em;
   ${'' /* border: 1px solid silver; */}
   @media (min-width: 768px) {    
     flex-direction: row;
+    margin: 4em 0;
   }
   `
   const Info = styled.div`
-    margin: 1em;
-    padding: 1em;
+    margin: 0 1em;
+    padding: 0 1em;
     ${'' /* display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -25,9 +27,10 @@ const Post = props => {
   `
   const Content = styled.div`
     display: flex;
-    margin: 1em;
-    padding: 1em;
+    margin: 0 1em;
+    padding: 0.5em 1em;
     align-items: center;
+    border-left: 2px dashed salmon;
   `
 
   return(
