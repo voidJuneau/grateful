a68df5c609e80f057e28bb52199381c0de01a474
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-21 22:15:58 -0400

    Fixed warning

--- client/src/components/Posts/PostList.js
+++ client/src/components/Posts/PostList.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Post from './Post';
 
 const PostList = props => {
-  return props.data.map(item => <Post data={item} />);
+  return props.data.map(item => <Post data={item} key={item._id} />);
 }
 
 export default PostList;
--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -5,10 +5,12 @@ import PostList from '../components/Posts/PostList';
 
 const Posts = props => {
   const POSTS = [{
+    _id: 3415125,
     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     ownerName: 'user0',
     date: Date().toString().substring(0, 15)
   }, {
+    _id: 34431,
     content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     ownerName: 'user1',
     date: Date().toString().substring(0, 15)
