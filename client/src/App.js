3b1c80a2ec0102049fdc56a5e368855c1a36391f
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-23 16:39:26 -0400

    Set up context base for later

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -10,38 +10,61 @@ import styled from 'styled-components';
 import Main from './pages/Main'
 import Posts from './pages/Posts'
 import Navigation from './components/Navigation/Navigation'
-import './App.css';
+import { AuthContext } from './utils/AuthContext';
 
-const App = () => {
+const App = () => {  
   const Container = styled.div`
     margin: 5em auto 0 auto;
     max-width: 768px;
   `
+  
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const auth = useContext(AuthContext);
 
+  const login = useCallback(() => setLoggedIn(true), []);
+  const logout = useCallback(() => setLoggedIn(false), []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <switch>
+        
+      </switch>
+    );
+  } else {
+    routes = (
+      <switch>
+
+      </switch>
+    );
+  }
+  
   return (
-    <Router>
-      <Navigation />
-      <Container>
-        <Switch>
-          <Route path="/" exact>
-            <Main />
-          </Route>
-          <Route path="/new">
+    <AuthContext.Provider>
+      <Router>
+        <Navigation />
+        <Container>
+          <Switch>
+            <Route path="/" exact>
+              <Main />
+            </Route>
+            <Route path="/new">
 
-          </Route>
-          <Route path="/all">
-            <Posts />
-          </Route>
-          <Route path="/me">
+            </Route>
+            <Route path="/all">
+              <Posts />
+            </Route>
+            <Route path="/me">
 
-          </Route>
-          <Route path="/auth">
+            </Route>
+            <Route path="/auth">
 
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </Container>
-    </Router>
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Container>
+      </Router>
+    </AuthContext.Provider>
   )
 }
 
