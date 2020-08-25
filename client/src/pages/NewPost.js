e9560813abc0ee6393c7e4a5548bda9c1bb6123e
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-24 21:22:51 -0400

    Temporarily finished new post page

--- client/src/pages/NewPost.js
+++ client/src/pages/NewPost.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import CenterContainer from '../components/shared/CenterContainer';
 import Card from '../components/shared/Card'
 import Button from '../components/shared/Button';
 
 const NewPost = () => {
+  const [content, setContent] = useState('');
+  const TextArea = styled.textarea`
+    width: 100%;
+  `
+
+  const savePost = event => {
+    event.preventDefault();
+    console.log(content)
+  }
+
   return (
     <CenterContainer>
       <Card>
         <h2>New Post</h2>
         <hr />
-        <form>
-          <Button>Having hard time on start?</Button>
-          <textarea></textarea>
+        <form onSubmit={savePost}>
+          {/* <Button>Having hard time on start?</Button> */}
+          <TextArea rows='5' value={content} onChange={e => setContent(e.target.value)}></TextArea>
           <Button type='submit'>Save</Button>
         </form>
       </Card>
