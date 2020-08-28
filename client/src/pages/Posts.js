3f1b1a4a66e7d933e60409895ab00dba6b96c34e
Juneau <33215669+voidJuneau@users.noreply.github.com>
2020-08-28 01:33:03 -0400

    Fixed infinite loop

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -6,16 +6,14 @@ import PostList from '../components/Posts/PostList';
 const Posts = props => {
   const [posts, setPosts] = useState();
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/post/all')
-    .then(res => setPosts(res.data))
-
-    if (!posts) {
-      return <div>asd</div>
-    } else {
-      return <PostList data={posts}/>
-    }
+  useEffect(() => {axios.get('http://localhost:5000/api/post/all')
+  .then(res => setPosts(res.data))
   }, [])
+  if (!posts) {
+    return <div>asd</div>
+  } else {
+    return <PostList data={posts}/>
+  }
 }
 
 export default Posts;
